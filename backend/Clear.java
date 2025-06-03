d89b97533354a7fa237b9479c774a41050e86482
waynexcellence <waynexcellence@gmail.com>
2025-06-03 18:01:10 +0800

    movie init data increase to 6 items

--- backend/Clear.java
+++ backend/Clear.java
@@ -18,7 +18,7 @@ public class Clear {
         Movie.truncate();
         Ticket.truncate();
 
-		if( Helper.getOneCharInput("create data by default? [y/n]: ", "yn") == 'y' ) {
+		if( Helper.getOneCharInput("要使用預設的資料嗎? [y/n]: ", "yn") == 'y' ) {
 			User user0 = new User(new Date(2000, 10, 10), "pass0", "mail0", true, 0);
 			User user1 = new User(new Date(2001, 11, 11), "pass1", "mail1", true, 1);
 			User.writeUser(user0);
@@ -33,15 +33,23 @@ public class Clear {
 			Film.writeFilm(film2);
 			Film.writeFilm(film3);
 
-			Theater theater0 = new Theater("large", Movie.largeSeatAmount, true, 0);
-			Theater theater1 = new Theater("small", Movie.smallSeatAmount, true, 1);
+			Theater theater0 = new Theater("大影廳", Movie.largeSeatAmount, true, 0);
+			Theater theater1 = new Theater("小影廳", Movie.smallSeatAmount, true, 1);
 			Theater.writeTheater(theater0);
 			Theater.writeTheater(theater1);
 
-			Movie movie0 = new Movie(film0, theater0, new Date(2020, 10, 10), 10, true, 0);
-			Movie movie1 = new Movie(film1, theater1, new Date(2021, 11, 11), 11, true, 1);
+			Movie movie0 = new Movie(film0, theater0, new Date(2025, 10, 10), 10, true, 0);
+			Movie movie1 = new Movie(film1, theater1, new Date(2025, 11, 11), 10, true, 1);
+			Movie movie2 = new Movie(film2, theater0, new Date(2025, 10, 10), 15, true, 2);
+			Movie movie3 = new Movie(film3, theater1, new Date(2025, 11, 11), 15, true, 3);
+			Movie movie4 = new Movie(film0, theater1, new Date(2025, 11, 11), 20, true, 4);
+			Movie movie5 = new Movie(film1, theater0, new Date(2025, 10, 10), 20, true, 5);
 			Movie.writeMovie(movie0);
 			Movie.writeMovie(movie1);
+			Movie.writeMovie(movie2);
+			Movie.writeMovie(movie3);
+			Movie.writeMovie(movie4);
+			Movie.writeMovie(movie5);
 
 			Ticket ticket0 = new Ticket(movie0.getUid(), user0.getUid(), new Seat('L', 12, Color.Back[3]), true, 0);
 			Ticket ticket1 = new Ticket(movie0.getUid(), user1.getUid(), new Seat('L', 13, Color.Back[3]), true, 1);
@@ -67,7 +75,7 @@ public class Clear {
 				System.out.println(item);
 			}
 		} else {
-        	System.out.println("usr, film, theater, movie, ticket, all clear.");
+        	System.out.println("usr, film, theater, movie, ticket, 已全部清空.");
 		}
 
         User.close();
---
+++ clear.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001
+echo 沒有錯誤訊息即可關閉
+java -Dfile.encoding=UTF-8 backend.Clear
+pause
